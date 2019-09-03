e4238b163a5ff0fbfb3965dfd0868b7d19ad7b9f
Kai-Xuan Chen <36260120+Kai-Xuan@users.noreply.github.com>
2019-09-03 22:57:51 +0800

    del

--- Tools/VL_DSIFT/DSIFT.m
+++
@@ -1,9 +0,0 @@
-function [DSIFT] = DSIFT(Img,Size,Step)
-%% Initialization
-magnif = 3 ;
-%% Operation
-Img = vl_imsmooth(single(Img),sqrt((Size/magnif)^2-.25));
-[~,DSIFT] = vl_dsift(single(Img),'size',Size,'step',Step);%,'FloatDescriptors'); 
-%% Finalization
-end
-
