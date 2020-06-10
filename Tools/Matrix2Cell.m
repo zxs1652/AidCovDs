5c8ab6bc74623445c177334e7cb3fdfef6e29952
Kai-Xuan Chen <36260120+Kai-Xuan@users.noreply.github.com>
2020-06-10 14:53:54 +0800

    del

--- Tools/Matrix2Cell.m
+++
@@ -1,6 +0,0 @@
-function Cell = Matrix2Cell(Matrix)
-    num = size(Matrix,3);
-    for i =1:num
-        Cell{1,i}=Matrix(:,:,i);       
-    end
-end
--- Tools/vlfeat-0.9.18/toolbox/sparse_coding/sc2/getObjective_sc.m
+++
@@ -1,9 +0,0 @@
-function [fobj, fresidue, freg] = getObjective_sc(A, S, X, beta)
-
-E = double(A)*S - double(X);
-fresidue = sum(sum(E.^2));
-freg = beta*sum(sum(abs(S)));
-
-fobj= fresidue + freg;
-
-
